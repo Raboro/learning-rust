7a29f4096eab6f1eeeb17b223ca82dc96d844b24
Raboro <marius.woerfelreal@gmail.com>
2024-02-27 22:38:30 +0100

    added Person struct with Display implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct Person {
+    name: String,
+    age: i8,
+}
+
+impl std::fmt::Display for Person {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{} with age: {}", self.name, self.age)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let person: Person = Person {
+        name: "Peter".to_string(),
+        age: 19,
+    };
+    println!("{}", person);
 }
