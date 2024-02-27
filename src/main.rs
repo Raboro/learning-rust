a1b5e5bb3fba86991428a1bbd5d1f35af8944391
Raboro <marius.woerfelreal@gmail.com>
2024-02-27 22:48:09 +0100

    added impl block with function for person

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,21 @@ struct Person {
     age: i8,
 }
 
+impl Person {
+    fn is_full_age(&self) -> bool {
+        self.age >= 18
+    }
+}
+
 impl std::fmt::Display for Person {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{} with age: {}", self.name, self.age)
+        write!(
+            f,
+            "{} with age: {} and is {}full age",
+            self.name,
+            self.age,
+            if self.is_full_age() { "" } else { "not" }
+        )
     }
 }
 
